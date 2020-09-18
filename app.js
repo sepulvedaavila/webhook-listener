2dbec17d89b6038bb50c4516266a84e5c69bfcc2
Carlos Sepulveda <carlossepulveda@MacBook-Air-de-Carlos.local>
2020-09-17 23:34:30 -0500

    Logging things

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var webhookRouter = require('./routes/webhook');
+var huepjutRouter = require('./routes/huepjuts');
 
 var app = express();
 
@@ -22,7 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/webhook', webhookRouter);
-app.use('/huepjuts', webhookRouter);
+app.use('/huepjuts', huepjutRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
--- routes/huepjuts.js
+++ routes/huepjuts.js
@@ -5,17 +5,17 @@ var router = express.Router();
 /* POST reqs logging . */
 router.post('/', function(req, res, next) {
 
-    var file = require("../var/log/whLog.txt"); 
+    var file = "./public/logs.txt";
     var data = req.body;
-    fs.writeFile(file, data, (err) => { 
+    var appending = "\n"+JSON.stringify(data);
+    fs.appendFile(file, appending, (err) => { 
         if (err) 
             console.log(err); 
         else { 
             console.log("Logs written successfully\n");
         } 
     }); 
-    //console.log(req.body);
-    //res.send(req.body);
+    res.send(data);
     
 });
 
