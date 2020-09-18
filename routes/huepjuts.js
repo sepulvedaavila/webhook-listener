09038307bfad944428da83afe322de7da6eecaf4
Carlos Sepulveda <carlossepulveda@MacBook-Air-de-Carlos.local>
2020-09-17 22:49:32 -0500

    Loggeo en la Consola

--- routes/huepjuts.js
+++ routes/huepjuts.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.post('/', function(req, res, next) {
-  res.send(req.body);
+    console.log(req.body);
+    res.send(req.body);
+  
 });
 
 module.exports = router;
