781c920750356db753340adeec74ef79505e936f
Carlos N. SepúlvedaAvila <cnsa1606@gmail.com>
2020-11-04 09:34:49 -0600

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var webhookRouter = require('./routes/webhook');
-var huepjutRouter = require('./routes/huepjuts');
 
 var app = express();
 
@@ -23,7 +22,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/webhook', webhookRouter);
-app.use('/huepjuts', huepjutRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
